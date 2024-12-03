3cbabea6bd2aabf6f17e37d274cc766df11eaa1c
Xolisa Nunge <xolisa@flume.co.za>
2024-12-03 08:26:50 +0200

    Configuring and setting up components on my page to test that they work

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,9 @@
-import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT(
-  {
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          background: "var(--background)",
-          foreground: "var(--foreground)",
-        },
-      },
-    },
-    plugins: [],
-  }satisfies Config
-);
-
-
+ 
+module.exports = withMT({
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
